6103ea0c150d54545c5882f0c075925b1617925d
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-25 15:08:55 +0800

    [OO P6][David Leung]add code for Practice 6

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,16 @@
 package practice06;
 
 public class Klass {
+    private int number;
+    public Klass(int number){
+        this.number=number;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+ this.number;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,43 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass(){
+        return this.klass;
+    }
+
+    public String introduceWith(Student student){
+        String result = super.introduce()+" I am a Teacher. ";
+        if(student.getKlass().getNumber()==this.getKlass().getNumber()){
+            result += "I teach ";
+        }
+        else{
+            result += "I don't teach ";
+        }
+        result += student.getName() +".";
+
+        return result;
+    }
+
+    @Override
+    public String introduce() {
+        String result = super.introduce()+" I am a Teacher. I teach ";
+        if(klass == null){
+            result += "No Class.";
+        }else{
+            result += this.klass.getDisplayName()+".";
+        }
+        return result;
+    }
 }
