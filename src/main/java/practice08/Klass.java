bd408bc62f43b8eb190930e2bc038f1f07408343
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-25 15:52:00 +0800

    [OO P8][David Leung]add code for practice 8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,40 @@
 package practice08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+
+    private int number;
+    private Student leader;
+    private List<Student> students = new ArrayList<Student>();
+
+    public Klass(int number){
+        this.number=number;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+
+    public void assignLeader(Student leader){
+        if(students.contains(leader)){
+            this.leader = leader;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        this.students.add(student);
+    }
+
+    public String getDisplayName(){
+        return "Class "+ this.number;
+    }
 }
