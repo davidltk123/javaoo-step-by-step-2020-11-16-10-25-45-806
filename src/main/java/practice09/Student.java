0abd747ef80ba436c4d553ffc91bb576021b4346
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-25 16:59:42 +0800

    [OO P9][David Leung]add code for practice 9

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,26 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass(){
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        String result = super.introduce() + " I am a Student. ";
+        if(this.klass.getLeader() != null && this.klass.getLeader().equals(this)){
+            result += "I am Leader of ";
+        }else{
+            result += "I am at ";
+        }
+        result += this.klass.getDisplayName() + ".";
+        return result;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,59 @@
 package practice09;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+    //private Klass klass;
+    private List<Klass> classes = new ArrayList<Klass>();
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public List<Klass> getClasses(){
+        return this.classes;
+    }
+
+    public String introduceWith(Student student){
+        String result = super.introduce()+" I am a Teacher. ";
+        if(this.classes.contains(student.getKlass())){
+            result += "I teach ";
+        }
+        else{
+            result += "I don't teach ";
+        }
+        result += student.getName() +".";
+
+        return result;
+    }
+
+    @Override
+    public String introduce() {
+        String result = super.introduce()+" I am a Teacher. I teach ";
+        if(this.classes.size() == 0){
+            result += "No Class.";
+        }else{
+            List<String> classNumbers  = this.classes.stream().map(item -> String.valueOf(item.getNumber())).collect(Collectors.toList());
+            String classesString = classNumbers.stream().reduce("Class ",(classString,item) -> {
+                if(classNumbers.indexOf(item)==classNumbers.size()-1){
+                    return classString+item+".";
+                } else{
+                    return classString+item+", ";
+                }
+            });
+            result += classesString;
+        }
+        return result;
+    }
+
+    public boolean isTeaching(Student student){
+        return this.classes.contains(student.getKlass());
+    }
 }
