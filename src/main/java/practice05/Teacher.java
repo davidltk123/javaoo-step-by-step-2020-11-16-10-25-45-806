92fe35dcd852fd9dc0410b2f6ad16e0138c57247
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-25 14:52:45 +0800

    [OO P5][David Leung]add code for practice 5

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass(){
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(klass == 0){
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }else{
+            return super.introduce()+" I am a Teacher. I teach Class "+this.klass+".";
+        }
+    }
 }
