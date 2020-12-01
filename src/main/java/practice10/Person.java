1399a23d1c07f08b21b6d136879a23022dc5483d
davidltk123 <davidltk123@gmail.com>
2020-12-01 23:12:05 +0800

    [OO Practice][David Leung] refactor: inline refactoring

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -34,10 +34,6 @@ public class Person {
         }
 
         final Person other = (Person) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-
-        return true;
+        return this.id == other.id;
     }
 }
